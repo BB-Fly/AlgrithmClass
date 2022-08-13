e9b737d2b11b8fc1976dea2932b76d592346f8f3
BB-Fly <97658029+BB-Fly@users.noreply.github.com>
2022-08-14 00:01:17 +0800

    Add files via upload

--- src/AlgorithmClass.cpp
+++ src/AlgorithmClass.cpp
@@ -1,7 +1,12 @@
-﻿#include"test_UnionFind.h"
+﻿#include"test.h"
 
 int main(){
-    test_unionfind();
-    test_BOM_unionfind();
+
+    //test_unionfind();
+    
+    //test_BOM_unionfind();
+
+    test_MonotonicStack();
+
     return 0;
 }
